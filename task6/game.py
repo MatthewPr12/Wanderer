ba574025e917e7bce0d00ba2cf1551bc6538d024
MatthewPr12 <matvii.prytula@ucu.edu.ua>
2022-04-15 19:03:50 +0300

    classes structured

--- task6/game.py
+++ task6/game.py
@@ -66,16 +66,143 @@ class Street:
         """
         return self.item
 
-    
-
-
-class Enemy:
-    pass
-
-
-class Friend:
-    pass
-
 
 class Character:
-    pass
+    def __init__(self, name, description, conversation=None):
+        self.name = name
+        self.description = description
+        self.conversation = conversation or ''
+
+    def set_conversation(self, text):
+        """
+        set the words for the character
+        :param text:
+        :return:
+        """
+        self.conversation = text
+
+    def describe(self):
+        """
+        show main info about the character
+        :return:
+        """
+        typ = type(self).__name__
+        print(f"{typ}: {self.name}",
+              f"Description: {self.description}", sep='\n')
+
+    def talk(self):
+        """
+        talk to the character
+        :return:
+        """
+        print(self.conversation)
+
+
+class Enemy(Character):
+    defeated = 0
+
+    def __init__(self, name, description, conversation=None, weakness=None):
+        super().__init__(name, description, conversation)
+        self.weakness = weakness or ''
+
+    def set_weakness(self, weak):
+        """
+        define what can enemy be killed with
+        :param weak:
+        :return:
+        """
+        self.weakness = weak
+
+    def fight(self, fight_with):
+        """
+        fight with the enemy providing you
+        got some proper tools
+        :param fight_with:
+        :return:
+        """
+        if fight_with == self.weakness:
+            return True
+        return False
+
+    def get_defeated(self):
+        """
+        command for enemy to die. keep the record
+        of killed enemies
+        :return:
+        """
+        Enemy.defeated += 1
+        return Enemy.defeated
+
+
+class Friend(Character):
+    def __init__(self, name, description, conversation=None, secret_gift=None):
+        super().__init__(name, description, conversation)
+        self.secret_gift = secret_gift
+
+    def coffee_time(self):
+        print("Let's go to the coffee house. You get to choose one",
+              "1) coffee manufactory",
+              "2) Lviv coffee mine",
+              "3) Aroma Kava", sep='\n')
+        choice = input("Enter a number: ")
+        if choice in {"1", "2"}:
+            print("Good choice")
+            if self.secret_gift:
+                print("Now I present to you the gift",
+                      "that will help you fight the boss")
+                return 0
+            return 1
+        elif choice == "3":
+            print("We're not friends anymore.",
+                  "Now I have to kill you for your tastes, pervert...")
+            return 2
+        print("Ugh, sorry, we don't go there. Bye")
+        return 3
+
+
+class Item:
+    """
+    Item class
+    """
+
+    def __init__(self, name, description=None):
+        """
+        constructor
+        :param name:
+        :param description:
+        """
+        self.name = name
+        self.description = description or ''
+
+    def set_description(self, text):
+        """
+        provide the item with main info about it
+        :param text:
+        :return:
+        """
+        self.description = text
+
+    def describe(self):
+        """
+        get the main info of the item
+        :return:
+        """
+        typ = type(self).__name__
+        print(f"{typ}: {self.name}",
+              f"Description: {self.description}", sep='\n')
+
+    def get_name(self):
+        """
+        get the name of the item
+        :return:
+        """
+        return self.name
+
+
+class Boss(Enemy):
+    def __init__(self, name, description, conversation):
+        super().__init__(name, description, conversation)
+        self.name = 'Russian-speaking baron'
+        self.description = 'extremely impudent creature'
+        self.weakness = 'Iryna Farion'
+        self.conversation = "We have a free country. What's the difference?"
